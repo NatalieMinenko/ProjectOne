a264e83ccaa4e409dfab134460b6dca151e4a4f2
N_Minenko <natalie-0909@mail.ru>
2024-08-18 14:49:39 +0300

    Init

---
+++ ProjectNogotochki/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectNogotochki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
