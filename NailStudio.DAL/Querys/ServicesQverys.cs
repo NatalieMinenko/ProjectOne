9225e957c69d794d93e35a69d796343936b270c3
Питенко София <pitenko.sofiya@mail.ru>
2024-08-27 19:47:56 +0300

    Create ServicesQvery

---
+++ NailStudio.DAL/Querys/ServicesQverys.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NailStudio.DAL.Querys
+{
+    public class ServicesQverys
+    {
+        public const string AddServicesQuery = $"INSERT INTO \"Services\"(\"Name\", \"Duration\", \"IsActive\", \"IsDeleted\")\r\n VALUES (@name, @duration, @isActive, @isDeleted);\r\n \";";
+        public const string GetAllServicesQuery = $"SELECT \" Id \",Name\", Duration\", IsActive\"IsDeleted\" FROM.public\"Services";
+        public const string UpdateServicesQuery = $"UPDATE\"Services\"SET\"Name\"= @name,\"Duration\"= @duration,\"IsActive\" = @isActive\"IsDeleted\" = @isDeleted\" WHERE \"Id\"= @id;";
+        public const string DeleteServicesQuery = $"DELETE FROM\"Services\"WHERE \"Id\"= @id;";
+
+    }
+}
