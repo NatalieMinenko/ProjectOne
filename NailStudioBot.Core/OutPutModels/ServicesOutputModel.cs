b97e0d52de8a3fef4b82d51605c6182221d1d4c1
Питенко София <pitenko.sofiya@mail.ru>
2024-08-27 21:36:59 +0300

    Create ServicesOutputModel

---
+++ NailStudioBot.Core/OutPutModels/ServicesOutputModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NailStudioBot.Core.OutPutModels
+{
+    public class ServicesOutputModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Duration { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
