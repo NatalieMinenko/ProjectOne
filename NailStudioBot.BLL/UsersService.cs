6bba3febc1fb50152146b28c8b8b0bae151d1afc
N_Minenko <natalie-0909@mail.ru>
2024-08-30 18:29:54 +0300

    Correct usersService

--- NailStudioBot.BLL/UsersService.cs
+++ NailStudioBot.BLL/UsersService.cs
@@ -9,46 +9,33 @@ namespace NailStudioBot.BLL
 {
     public class UsersService
     {
+        public UserRepository UserRepository { get; set; }
+
+        private Mapper _mapper;
+
+        public UsersService()
+        {
+            UserRepository = new UserRepository();
+
+            var config = new MapperConfiguration(
+                cfg => {
+                    cfg.AddProfile(new UsersMappingProfile());
+                });
+            _mapper = new Mapper(config);
+        }
+
         public void AddUser(UsersInputModel user)
         {
-            throw new NotImplementedException();
+            var userDto = _mapper.Map<UsersDto>(user);
+
+            UserRepository.AddUser(userDto.Name);
         }
 
-        public IEnumerable<object> GetAllUsers()
+        public List<UsersOutputModel> GetAllUsers()
         {
-            throw new NotImplementedException();
-        }
-
-        public class UserService
-        {
-            public UserRepository UserRepository { get; set; }
-
-            private Mapper _mapper;
-
-            public UserService()
-            {
-                UserRepository = new UserRepository();
-
-                var config = new MapperConfiguration(
-                    cfg => {
-                        cfg.AddProfile(new UsersMappingProfile());
-                    });
-                _mapper = new Mapper(config);
-            }
-
-            public void AddUser(UsersInputModel user)
-            {
-                var userDto = _mapper.Map<UsersDto>(user);
-
-                UserRepository.AddUser(userDto.Name);
-            }
-
-            public List<UsersOutputModel> GetAllUsers()
-            {
-                List<UsersDto> userDtos = UserRepository.GetAllUsers();
-                List<UsersOutputModel> users = _mapper.Map<List<UsersOutputModel>>(userDtos);
-                return users;
-            }
+            List<UsersDto> userDtos = UserRepository.GetAllUsers();
+            List<UsersOutputModel> users = _mapper.Map<List<UsersOutputModel>>(userDtos);
+            return users;
         }
 
     }
