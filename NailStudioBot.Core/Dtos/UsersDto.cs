d3f6906ad9911f34bd9840ce1abf886674eec821
N_Minenko <natalie-0909@mail.ru>
2024-09-01 16:02:17 +0300

    Dtos

--- NailStudioBot.Core/Dtos/UsersDto.cs
+++ NailStudioBot.Core/Dtos/UsersDto.cs
@@ -9,7 +9,16 @@ namespace NailStudioBot.Core.Dtos
     public class UsersDto
     {
         public int? Id { get; set; }
+
         public string? Name { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? Mail { get; set; }
+
+        public int? RoleId { get; set; }
+
+        public int? MasterTypeId { get; set; }
         
         public int? UserCount { get; set; }
     }
