9344a3c8e0c4ea14f87ae089fa76fc1239791cd5
Питенко София <pitenko.sofiya@mail.ru>
2024-09-02 23:18:34 +0300

    Add reservation Repository

--- NailStudio.DAL/ReservationsRepository.cs
+++ NailStudio.DAL/ReservationsRepository.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Dapper;
+using NailStudio.DAL.Querys;
+using NailStudioBot.Core;
+using NailStudioBot.Core.Dtos;
+using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,76 @@ using System.Threading.Tasks;
 
 namespace NailStudio.DAL
 {
-    internal class ReservationsRepository
+    public class ReservationsRepository
     {
+        public void InsertReservation(ReservationsDto reservation)
+        {
+            string conectionString = Options.ConectionString;
+            using (var connection = new NpgsqlConnection(conectionString))
+            {
+                string query = ReservationsQuerys.InsertReservationQuery;
+                var args = new
+                {
+                    reservation.StartDateTime,
+                    reservation.Sum,
+                    reservation.ClientId,
+                    reservation.StatusId,
+                    reservation.IsAction
+                };
+
+                connection.Open();
+                connection.Query(query, args);
+            }
+        }
+
+        public List<ReservationsDto> GetAllReservations()
+        {
+            string conectionString = Options.ConectionString;
+            using (var connection = new NpgsqlConnection(conectionString))
+
+            {
+                string query = ReservationsQuerys.GetAllReservationsQuery;
+
+                connection.Open();
+                return connection.Query<ReservationsDto>(query).ToList();
+            }
+        }
+
+        public void UpdateReservation(ReservationsDto reservation)
+        {
+            string conectionString = Options.ConectionString;
+            using (var connection = new NpgsqlConnection(conectionString))
+            {
+                string query = ReservationsQuerys.UpdateReservationQuery;
+                var args = new
+                {
+                    reservation.StartDateTime,
+                    reservation.Sum,
+                    reservation.ClientId,
+                    reservation.StatusId,
+                    reservation.IsAction,
+                    reservation.Id
+                };
+
+                connection.Open();
+                connection.Execute(query, args);
+            }
+        }
+
+        public void DeleteReservationById(int id)
+        {
+            string conectionString = Options.ConectionString;
+            using (var connection = new NpgsqlConnection(conectionString))
+            {
+                string query = ReservationsQuerys.DeleteReservationByIdQuery;
+                var args = new { id };
+
+                connection.Open();
+                connection.Execute(query, args);
+            }
+        }
+
+
+
     }
 }
