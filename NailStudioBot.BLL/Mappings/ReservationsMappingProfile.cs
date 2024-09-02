e5a48a851b1f3b9b0f489f2d86796e02347f09ea
Питенко София <pitenko.sofiya@mail.ru>
2024-09-03 00:04:34 +0300

    Add ReservationsMappingProfile

---
+++ NailStudioBot.BLL/Mappings/ReservationsMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using NailStudioBot.Core.Dtos;
+using NailStudioBot.Core.InputModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NailStudioBot.BLL.Mappings
+{
+    public class ReservationsMappingProfile:Profile
+    {
+        public ReservationsMappingProfile()
+        {
+            CreateMap<ReservationsInputModel, ReservationsDto>();
+            CreateMap<ReservationsDto, ReservationsInputModel>();
+        }
+    }
+}
