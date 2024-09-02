af252c8a8c7f3af9ce941ee51de7c1c37419fe7c
Питенко София <pitenko.sofiya@mail.ru>
2024-09-02 23:01:52 +0300

    Correct ServicesQuerys

--- NailStudio.DAL/Querys/ServicesQuerys.cs
+++ NailStudio.DAL/Querys/ServicesQuerys.cs
@@ -8,23 +8,24 @@ namespace NailStudio.DAL.Querys
 {
     public class ServicesQuerys
     {
-        public const string AddServicesQuery = $"INSERT INTO \"Services\"(\"Name\", \"Duration\", \"IsActive\"," +
-            $" \"IsDeleted\")\r\n VALUES (@name, @duration, @isActive, @isDeleted);\r\n \";";
+        public const string AddServicesQuery = "INSERT INTO \"Services\" (\"Name\", \"Duration\", \"IsActive\", \"IsDeleted\") " +
+         "VALUES (@name, @duration, @isActive, @isDeleted);";
 
-        public const string GetAllServicesQuery = $"SELECT \" Id \",Name\", Duration\", IsActive\"IsDeleted\" FROM.public\"Services";
+        public const string GetAllServicesQuery = "SELECT \"Id\", \"Name\", \"Duration\", \"IsActive\", \"IsDeleted\" FROM \"Services\";";
 
-        public const string UpdateServicesQuery = $"UPDATE\"Services\"SET\"Name\"= @name,\"Duration\"= @duration," +
-            $"\"IsActive\" = @isActive\"IsDeleted\" = @isDeleted\" WHERE \"Id\"= @id;";
+        public const string UpdateServicesQuery = "UPDATE \"Services\" SET \"Name\" = @name, \"Duration\" = @duration, " +
+             "\"IsActive\" = @isActive, \"IsDeleted\" = @isDeleted WHERE \"Id\" = @id;";
 
-        public const string DeleteServicesQuery = $"DELETE FROM\"Services\"WHERE \"Id\"= @id;";
+        public const string DeleteServicesQuery = "DELETE FROM \"Services\" WHERE \"Id\" = @id;";
 
-        public const string FindMasterByServiceIdQuery = $"select S.\"Id\", S.\"Name\", S.\"Duration\", MTS.\"Cost\", MT.\"Id\"," +
-            $" MT.\"Name\", U.\"Id\", U.\"Name\"\r\n" +
-            $"from \"Services\" as S\r\n join \"MasterType_Services\" as MTS on MTS.\"ServiceId\"= S.\"Id\"\r\n" +
-            $"join \"MasterTypes\" as MT on MT.\"Id\" = MTS.\"MasterTypeId\"\r\n" +
-            $"join \"Users\" as U on U.\"MasterTypeId\" = MT.\"Id\"\r\n where S.\"Id\" = @id;";
+        public const string FindMasterByServiceIdQuery = "SELECT S.\"Id\", S.\"Name\", S.\"Duration\", MTS.\"Cost\", MT.\"Id\", " +
+           "MT.\"Name\", U.\"Id\", U.\"Name\" " +
+           "FROM \"Services\" AS S " +
+           "JOIN \"MasterType_Services\" AS MTS ON MTS.\"ServiceId\" = S.\"Id\" " +
+           "JOIN \"MasterTypes\" AS MT ON MT.\"Id\" = MTS.\"MasterTypeId\" " +
+           "JOIN \"Users\" AS U ON U.\"MasterTypeId\" = MT.\"Id\" " +
+           "WHERE S.\"Id\" = @id;";
 
-        public const string NameAllServicesQuery = $"select \"Name\" from \"Services\"\r\n where \"IsActive\" = true " +
-            $"and \"IsDeleted\"=false;";
+        public const string NameAllServicesQuery = "SELECT \"Name\" FROM \"Services\"WHERE \"IsActive\" = true AND \"IsDeleted\" = false;";
     }
 }
