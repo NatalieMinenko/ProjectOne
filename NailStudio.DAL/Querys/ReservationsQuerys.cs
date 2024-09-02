eba32d6d78a38bf4f49d151afd529d82a7c98883
Питенко София <pitenko.sofiya@mail.ru>
2024-09-02 22:56:12 +0300

    Add querys Reservations

--- NailStudio.DAL/Querys/ReservationsQuerys.cs
+++ NailStudio.DAL/Querys/ReservationsQuerys.cs
@@ -8,12 +8,16 @@ namespace NailStudio.DAL.Querys
 {
     public class ReservationsQuerys
     {
-        public const string InsertReservationQuery = $"INSERT INTO \"Reservations\"(StartDateTime\", Sum\", ClientId\",StatusId\",IsAction\")" +
-            $"VALUES (@StartDateTime, @Sum, @ClientId, @StatusId, @IsAction);";
-        public const string GetAllReservationsQuery = $"SELECT\"Id\", StartDateTime\",Sum\",ClientId\",StatusId\",IsAction\"FROM public.Reservations\";";
+        public const string InsertReservationQuery = $"INSERT INTO \"Reservations\"(\"StartDateTime\", \"Sum\", \"ClientId\", \"StatusId\", \"IsAction\")" +
+            $" VALUES (@StartDateTime, @Sum, @ClientId, @StatusId, @IsAction);";
+        public const string GetAllReservationsQuery = $"SELECT \"Id\", \"StartDateTime\", \"Sum\", \"ClientId\", \"StatusId\", \"IsAction\" FROM \"Reservations\";";
 
-        public const string UpdateReservationQuery = $"UPDATE\"Reservations\"SET StartDateTime\" = @StartDateTime,\"Sum= @Sum," +
-            $" ClientId = @ClientId,StatusId\" = @StatusId,\"IsAction\" = @IsAction  WHERE \"Id = @Id;";
+        public const string UpdateReservationQuery = $"UPDATE \"Reservations\" SET \"StartDateTime\" = @StartDateTime, \"Sum\" = @Sum, " +
+            $"\"ClientId\" = @ClientId, \"StatusId\" = @StatusId, \"IsAction\" = @IsAction WHERE \"Id\" = @Id;";
+
+        public const string DeleteReservationByIdQuery = $"DELETE FROM \"Reservations\" WHERE \"Id\" = @Id;";
+
+
 
 
 
