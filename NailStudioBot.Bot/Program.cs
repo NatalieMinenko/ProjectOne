89ac5728b3612567d42bfef3f02bee5444f22930
Питенко София <pitenko.sofiya@mail.ru>
2024-09-03 19:45:08 +0300

    Add SingleToneStorege

--- NailStudioBot.Bot/Program.cs
+++ NailStudioBot.Bot/Program.cs
@@ -6,154 +6,227 @@ using NailStudioBot.Core.InputModels;
 using NailStudioBot.BLL;
 using NailStudio.DAL;
 using System.Xml.Linq;
-
+using NailStudioBot.Bot.States.AdminStates;
 
 namespace NailStudioBot.Bot
 
-        
+
 
 {
     public class Program
     {
-        
 
-        public static Dictionary<long, string> States { get; set; }
-
-        private static UserService _userService;
-
-        public static void Main(string[] args)
+        private SingleToneStorage _storage;
+        static void Main(string[] args)
         {
-            States = new Dictionary<long, string>();
 
-            _userService = new UserService();
-
-            ITelegramBotClient bot = new TelegramBotClient("6997618609:AAEIBEGrUEXn7LXUK5y33vjjPmaMsGfs6SQ");
-
+            ITelegramBotClient client = SingleToneStorage.GetStorage().Client;
             var cts = new CancellationTokenSource();
             var cancellationToken = cts.Token;
 
-            var receiverOptions = new ReceiverOptions
+            var receiverOptions = new ReceiverOptions()
             {
-                AllowedUpdates = {}
+                AllowedUpdates = [UpdateType.Message, UpdateType.CallbackQuery],
+                ThrowPendingUpdates = true
             };
 
-            bot.StartReceiving(
-                HandleUpdateAsync,
-                HandleErrorAsync,
-                receiverOptions,
-                cancellationToken
-            );
+            client.StartReceiving
+                    (
+                    HandleUpdate,
+                    HandleError,
+                    receiverOptions,
+                    cancellationToken
+                    );
 
-            Console.WriteLine("ЗАРАБОТАЛО!!!");
-
-            Console.ReadLine();
+            Console.WriteLine("я работаю");
+            string end;
+            do
+            {
+                end = Console.ReadLine();
+            } while (end != "end");
         }
 
-   
-
-        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        private static async Task HandleUpdate(ITelegramBotClient client, Update update, CancellationToken token)
         {
-            if (update.Type == UpdateType.Message)
+            var users = SingleToneStorage.GetStorage().Clients;
+            long id = 0;
+            switch (update.Type)
             {
-                var message = update.Message;
-                var id = message.Chat.Id;
-                if (!States.ContainsKey(id))
-                {
-                    States[id] = "start";
-                }
-
-                if (message.Text.ToLower() == "/start")
-                {
-                    await botClient.SendTextMessageAsync(message.Chat, "Ну я заработал, короч");
-                    await botClient.SendTextMessageAsync(message.Chat, $"Тебя зовут {message.Chat.FirstName}");
-
-                    if (!States.ContainsKey(message.Chat.Id))
-                    {
-                        States.Add(message.Chat.Id, "start");
-                    }
-
-                }
-                else if (message.Text == "1")
-                {
-                    States[id] = "add";
-                    await botClient.SendTextMessageAsync(message.Chat, "Введите имя!");
-                }
-                else if (message.Text == "add")
-                {
-                    //тут будем добавлять юзера
-                    var user = new UsersInputModel()
-                    {
-                        Name = message.Text,
-                        
-                    };
-
-                    _userService.AddUser(user);
-
-                    States[id] = "start";
-                }
-                else if (message.Text == "2")
-                {
-                    var users = _userService.GetAllUsers();
-                    string res = "";
-
-                    foreach (var user in users)
-                    {
-                        res += $@"{user.Id} - {user.Name}
-";
-                    }
-
-                    await botClient.SendTextMessageAsync(message.Chat, res);
-                }
-                else if (message.Text == "3")
-                {
-                    States[id] = "update";
-                    await botClient.SendTextMessageAsync(message.Chat, "Введите ID пользователя, которого хотите обновить:");
-                }
-                else if (States[id] == "update")
-                {
-                    if (int.TryParse(message.Text, out int userId))
-                    {
-                        var user = _userService.GetUsersById(userId);
-                        if (user != null)
-                        {
-                            States[id] = "update_name";
-                            await botClient.SendTextMessageAsync(message.Chat, "Введите новое имя:");
-                        }
-                        else
-                        {
-                            await botClient.SendTextMessageAsync(message.Chat, "Пользователь с таким ID не найден.");
-                            States[id] = "start";
-                        }
-                    }
+                case UpdateType.Unknown:
+                    break;
+                case UpdateType.Message:
+                    id = update.Message.Chat.Id;
+                    if (!users.ContainsKey(id))
+                        users.Add(id, new StartState());
                     else
                     {
-                        await botClient.SendTextMessageAsync(message.Chat, "Неверный формат ID.");
-                        States[id] = "start";
+                        users[id] = users[id].ReceiveMessage(update);
                     }
-                }
-                else if (States[id] == "update_name")
-                {
-                    var user = new UsersInputModel()
+                    if (update.Type == UpdateType.Message)
                     {
-                        Id = userId,
-                        Name = message.Text,
-                    };
+                        var message = update.Message.Text.ToLower();
+                        if (message == "/admin")
+                        {
+                            users[id] = new AdminState();
+                        }
+                    }
 
-                    _userService.UpdateUser(user);
-                    await botClient.SendTextMessageAsync(message.Chat, "Пользователь обновлен!");
-                    States[id] = "start";
-                }
-
-
-
+                    break;
+                case UpdateType.InlineQuery:
+                    break;
+                case UpdateType.ChosenInlineResult:
+                    break;
+                case UpdateType.CallbackQuery:
+                    id = update.CallbackQuery.Message.Chat.Id;
+                    var c = users[id].ReceiveMessage(update);
+                    users[id] = c;
+                    break;
+                default:
+                    break;
             }
+            users[id].SendMessage(id);
         }
 
-
-        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        private static async Task HandleError(ITelegramBotClient client, Exception exception, CancellationToken token)
         {
-            Console.WriteLine(exception.ToString());
+            throw new NotImplementedException();
         }
+    }
+  
+        //public static void HandleError(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        //{
+
+        //}
+
+        //        public static Dictionary<long, string> States { get; set; }
+
+        //        private static UserService _userService;
+
+        //        public static void Main(string[] args)
+        //        {
+        //            States = new Dictionary<long, string>();
+
+        //            _userService = new UserService();
+
+        //            ITelegramBotClient bot = new TelegramBotClient("6997618609:AAEIBEGrUEXn7LXUK5y33vjjPmaMsGfs6SQ");
+
+        //            var cts = new CancellationTokenSource();
+        //            var cancellationToken = cts.Token;
+
+        //            var receiverOptions = new ReceiverOptions
+        //            {
+        //                AllowedUpdates = {}
+        //            };
+
+        //            bot.StartReceiving(
+        //                HandleUpdateAsync,
+        //                HandleErrorAsync,
+        //                receiverOptions,
+        //                cancellationToken
+        //            );
+
+        //            Console.WriteLine("ЗАРАБОТАЛО!!!");
+
+        //            Console.ReadLine();
+        //        }
+
+
+
+        //        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        //        {
+        //            if (update.Type == UpdateType.Message)
+        //            {
+        //                var message = update.Message;
+        //                var id = message.Chat.Id;
+        //                if (!States.ContainsKey(id))
+        //                {
+        //                    States[id] = "start";
+        //                }
+
+        //                if (message.Text.ToLower() == "/start")
+        //                {
+        //                    await botClient.SendTextMessageAsync(message.Chat, "Ну я заработал, короч");
+        //                    await botClient.SendTextMessageAsync(message.Chat, $"Тебя зовут {message.Chat.FirstName}");
+
+        //                    if (!States.ContainsKey(message.Chat.Id))
+        //                    {
+        //                        States.Add(message.Chat.Id, "start");
+        //                    }
+
+        //                }
+        //                else if (message.Text == "1")
+        //                {
+        //                    States[id] = "add";
+        //                    await botClient.SendTextMessageAsync(message.Chat, "Введите имя!");
+        //                }
+        //                else if (message.Text == "add")
+        //                {
+        //                    //тут будем добавлять юзера
+        //                    var user = new UsersInputModel()
+        //                    {
+        //                        Name = message.Text,
+
+        //                    };
+
+        //                    _userService.AddUser(user);
+
+        //                    States[id] = "start";
+        //                }
+        //                else if (message.Text == "2")
+        //                {
+        //                    var users = _userService.GetAllUsers();
+        //                    string res = "";
+
+        //                    foreach (var user in users)
+        //                    {
+        //                        res += $@"{user.Id} - {user.Name}
+        //";
+        //                    }
+
+        //                    await botClient.SendTextMessageAsync(message.Chat, res);
+        //                }
+        //else if (message.Text == "3")
+        //{
+        //    States[id] = "update";
+        //    await botClient.SendTextMessageAsync(message.Chat, "Введите ID пользователя, которого хотите обновить:");
+        //}
+        //else if (States[id] == "update")
+        //{
+        //    if (int.TryParse(message.Text, out int userId))
+        //    {
+        //        var user = _userService.GetUsersById(userId);
+        //        if (user != null)
+        //        {
+        //            States[id] = "update_name";
+        //            await botClient.SendTextMessageAsync(message.Chat, "Введите новое имя:");
+        //        }
+        //        else
+        //        {
+        //            await botClient.SendTextMessageAsync(message.Chat, "Пользователь с таким ID не найден.");
+        //            States[id] = "start";
+        //        }
+        //    }
+        //    else
+        //    {
+        //        await botClient.SendTextMessageAsync(message.Chat, "Неверный формат ID.");
+        //        States[id] = "start";
+        //    }
+        //}
+        //else if (States[id] == "update_name")
+        //{
+        //    var user = new UsersInputModel()
+        //    {
+        //        //Id = userId,
+        //        Name = message.Text,
+        //    };
+
+        //    _userService.UpdateUser(user);
+        //    await botClient.SendTextMessageAsync(message.Chat, "Пользователь обновлен!");
+        //    States[id] = "start";
+        //}
+
+
 
     }
-}
+    
+
