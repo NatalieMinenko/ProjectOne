7c0fd3ff4d792090839ec2ec792bb9f38553d7ad
Питенко София <pitenko.sofiya@mail.ru>
2024-09-10 17:06:13 +0300

    Add States

---
+++ NailStudioBot.Bot/States/AbstractState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace NailStudioBot.Bot.Statettes
+{
+    public abstract class AbstractState
+    {
+        public abstract void HandleMessage(Context context, Update update);
+
+        public abstract void ReactInBot(Context context, ITelegramBotClient botClient);
+    }
+}
---
+++ NailStudioBot.Bot/States/AdminState/LoginAdminState.cs
@@ -0,0 +1,40 @@
+﻿using NailStudioBot.Bot.States.AdminState;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace NailStudioBot.Bot.Statettes.AdmonState
+{
+    public class LoginAdminState : AbstractState
+    {
+        private  string AdminPassword = "111";
+        public override void HandleMessage(Context context, Update update)
+        {
+            if (update.Type == UpdateType.Message)
+            {
+                var message = update.Message;
+                if (message.Text.ToLower() == AdminPassword)
+                {
+                    context.State = new AdminMenuState();
+                }
+                else 
+                {
+                    context.State = new LoginAdminState();
+                }
+
+                
+            }
+        }
+
+        public override void ReactInBot(Context context, ITelegramBotClient botClient)
+        {
+                 botClient.SendTextMessageAsync(new ChatId(context.ChatId), "Введите пароль");
+
+        }
+    }
+}
