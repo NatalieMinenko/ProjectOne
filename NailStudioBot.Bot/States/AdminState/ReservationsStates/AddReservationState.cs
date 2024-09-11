c2ebecce044c109313ef50df98e1fd638a4cdbfd
Питенко София <pitenko.sofiya@mail.ru>
2024-09-12 01:42:49 +0300

    Add Reservations States from Admin

---
+++ NailStudioBot.Bot/States/AdminState/ReservationsStates/AddReservationState.cs
@@ -0,0 +1,38 @@
+﻿using NailStudioBot.Bot.States.AdminState.MasterOperationStates;
+using NailStudioBot.Bot.Statettes;
+using NailStudioBot.Core.InputModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace NailStudioBot.Bot.States.AdminState.ReservationsStates
+{
+    public class AddReservationState : AbstractState
+    {
+        public override void HandleMessage(Context context, Update update)
+        {
+            var reservation = new ReservationsInputModel();
+
+            //if (DateTime.TryParse(update.Message.Text, out DateTime startDateTime))
+            //{
+            //    reservation.StartDateTime = startDateTime.ToString(); 
+            //    context.State = new AddMasterPhone(reservation);
+            //}
+            //else
+            //{
+
+            //    context.BotClient.SendTextMessageAsync("Пожалуйста, введите корректную дату и время в формате (например, 2023-10-10 14:00).");
+            //}
+        }
+        public override void ReactInBot(Context context, ITelegramBotClient botClient)
+        {
+            botClient.SendTextMessageAsync(context.ChatId, "Введите дату и время записи");
+        }
+    }
+
+    }
+
---
+++ NailStudioBot.Bot/States/AdminState/ReservationsStates/AdminReservationsState.cs
@@ -0,0 +1,72 @@
+﻿using NailStudioBot.Bot.States.AdminState.MasterOperationStates;
+using NailStudioBot.Bot.Statettes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace NailStudioBot.Bot.States.AdminState.ReservationsStates
+{
+    public class AdminReservationsState : AbstractState
+    {
+        public override void HandleMessage(Context context, Update update)
+        {
+            switch (update.CallbackQuery.Data)
+            {
+                case "1":
+                    context.State = new UpdateResetvationState();
+                    break;
+                case "2":
+                    context.State = new AddReservationState();
+                    break;
+                case "3":
+                    context.State = new DeleteReservationState();
+                    break;
+                case "4":
+                    context.State = new GetAllReservationsState();
+                    break;
+                case "5":
+                    context.State = new AdminMenuState();
+                    break;
+                default:
+                    break;
+            }
+
+
+        }
+
+        public override void ReactInBot(Context context, ITelegramBotClient botClient)
+        {
+
+
+            // using Telegram.Bot.Types.ReplyMarkups;
+
+            InlineKeyboardMarkup markup = new InlineKeyboardMarkup(
+                   new InlineKeyboardButton[][]
+                   {
+                        new InlineKeyboardButton[]
+                        {
+                            new InlineKeyboardButton("Редактировать запись") { CallbackData="1"},
+                            new InlineKeyboardButton("Добавить запись") { CallbackData="2"},
+                             new InlineKeyboardButton("Удалить запись") { CallbackData="3"},
+                        },
+                        new InlineKeyboardButton[]
+                        {
+                            new InlineKeyboardButton("Посмотреть все записи") { CallbackData="4"},
+                            new InlineKeyboardButton("Вернуться в меню") { CallbackData="5"},
+
+
+                        }
+                   }
+                   );
+
+            var sent = botClient.SendTextMessageAsync(context.ChatId, "Choose a response", replyMarkup: markup);
+
+
+        }
+    }
+}
