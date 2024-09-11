4e93e1ae15cc43e6f10d06f69047a88a6fe7f6e4
N_Minenko <natalie-0909@mail.ru>
2024-09-11 19:06:55 +0300

    LoginMasterState

---
+++ NailStudioBot.Bot/States/MasterStates/LoginMasterState.cs
@@ -0,0 +1,38 @@
+﻿using NailStudioBot.Bot.States.AdminState;
+using NailStudioBot.Bot.Statettes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace NailStudioBot.Bot.States.MasterStates
+{
+    public class LoginMasterState : AbstractState
+    {
+        private string MasterPassword = "222";
+        public override void HandleMessage(Context context, Update update)
+        {
+            if (update.Type == UpdateType.Message)
+            {
+                var message = update.Message;
+                if (message.Text.ToLower() == MasterPassword)
+                {
+                    context.State = new MasterMenuState();
+                }
+                else
+                {
+                    context.State = new LoginMasterState();
+                }
+            }
+        }
+
+        public override void ReactInBot(Context context, ITelegramBotClient botClient)
+        {
+            botClient.SendTextMessageAsync(new ChatId(context.ChatId), "Введите пароль");
+        }
+    }
+}
