220edbf64b0e7eb9b8529aa123480f904c7b4b17
N_Minenko <natalie-0909@mail.ru>
2024-09-11 19:06:18 +0300

    MasterMenuStates

---
+++ NailStudioBot.Bot/States/MasterStates/MasterMenuState.cs
@@ -0,0 +1,50 @@
+﻿using NailStudioBot.Bot.States.AdminState.MasterOperationStates;
+using NailStudioBot.Bot.States.MasterStates.MasterServicesStates;
+using NailStudioBot.Bot.States.AdminState;
+using NailStudioBot.Bot.Statettes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace NailStudioBot.Bot.States.MasterStates
+{
+    public class MasterMenuState : AbstractState
+    {
+        public override void HandleMessage(Context context, Update update)
+        {
+            if (update.CallbackQuery.Data == "1")
+            {
+                context.State = new ShowActiveReservationsState();
+            }
+            else if (update.CallbackQuery.Data == "2")
+            {
+
+                context.State = new ShowAllReservationsState();
+            }
+            //return replyState;
+        }
+        public override void ReactInBot(Context context, ITelegramBotClient botClient)
+        {
+            // using Telegram.Bot.Types.ReplyMarkups;
+
+            InlineKeyboardMarkup markup = new InlineKeyboardMarkup(
+                   new InlineKeyboardButton[][]
+                   {
+                        new InlineKeyboardButton[]
+                        {
+                            new InlineKeyboardButton("Активные записи") { CallbackData="1"},
+                            new InlineKeyboardButton("Все записи") { CallbackData="2"},
+                        }
+                   }
+                   );
+
+            var sent = botClient.SendTextMessageAsync(context.ChatId, "Choose a response", replyMarkup: markup);
+
+        }
+    }
+}
