b706fe5072cbb38612e05f6ef2761cd91e8b3691
N_Minenko <natalie-0909@mail.ru>
2024-09-11 19:07:11 +0300

    AllReservations

---
+++ NailStudioBot.Bot/States/MasterStates/MasterServicesStates/ShowAllReservationsState.cs
@@ -0,0 +1,37 @@
+﻿using NailStudioBot.BLL;
+using NailStudioBot.Bot.Statettes;
+using NailStudioBot.Core.InputModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace NailStudioBot.Bot.States.MasterStates.MasterServicesStates
+{
+    public class ShowAllReservationsState : AbstractState
+    {
+        public override void HandleMessage(Context context, Update update)
+        {
+            
+        }
+
+        public override async void ReactInBot(Context context, ITelegramBotClient botClient)
+        {
+            var reservations = new ReservationsServices().GetAllReservations();
+
+            string res = "";
+
+            foreach (var reservation in reservations)
+            {
+                res += $"{reservations.Id}-{reservations.Name}-{reservations.Cost}\n";
+            }
+
+            await botClient.SendTextMessageAsync(new ChatId(context.ChatId), res);
+
+            context.State = new MasterMenuState();
+        }
+    }
+}
