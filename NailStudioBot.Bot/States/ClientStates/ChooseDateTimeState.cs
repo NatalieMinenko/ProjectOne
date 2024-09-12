c606bbfef3fa6c5ce2800b873acd5f13ab75681d
Сергей Воробей <flyaka1991@mail.ru>
2024-09-13 02:55:03 +0300

    Add CLientDateTimeState

---
+++ NailStudioBot.Bot/States/ClientStates/ChooseDateTimeState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using NailStudioBot.Bot.Statettes;
+
+namespace NailStudioBot.Bot.States.ClientStates
+{
+    public class ChooseDateTimeState : AbstractState
+    {
+        public override void HandleMessage(Context context, Update update)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ReactInBot(Context context, ITelegramBotClient botClient)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
