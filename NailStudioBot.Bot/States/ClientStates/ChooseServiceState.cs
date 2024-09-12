c015463cbd9afcceb389a1f322c24e6af72bf769
Сергей Воробей <flyaka1991@mail.ru>
2024-09-13 02:57:19 +0300

    Add ClientChooseServiceState

---
+++ NailStudioBot.Bot/States/ClientStates/ChooseServiceState.cs
@@ -0,0 +1,24 @@
+﻿using NailStudioBot.Bot.Statettes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace NailStudioBot.Bot.States.ClientStates
+{
+    public class ChooseServiceState : AbstractState
+    {
+        public override void HandleMessage(Context context, Update update)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ReactInBot(Context context, ITelegramBotClient botClient)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
